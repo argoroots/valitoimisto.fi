025ca3c9eaa2e76845d38c72407f542476a26c3a
Argo Roots <argo@roots.ee>
2023-11-29 15:39:06 +0200

    Add disclaimer to calculator

--- pages/calculator.vue
+++ pages/calculator.vue
@@ -1,3 +1,10 @@
 <template>
-  <div />
+  <div class="h-full flex flex-col">
+    <div class="grow">
+      .
+    </div>
+    <p class="italic text-sm">
+      Kalkulaator on indikatiivne ja annab esialgse vastuse teiepoolt sisestatud andmetega. Täpse arvutuse teeb raamatupidaja ja seda teiepoolt saadetud ja saadud isikuandmete alusel ning olenevalt vanusest ja maksuprotsentidest võib kalkulaatoris saadud summa erineda.
+    </p>
+  </div>
 </template>
