b7eb004c28c71af4b8fbe7410c73d9be9871c85b
Argo Roots <argo@roots.ee>
2023-11-29 15:58:36 +0200

    Group send form fields

--- pages/send.vue
+++ pages/send.vue
@@ -3,90 +3,115 @@ useHead({ title: 'Saada andmed' })
 </script>
 
 <template>
-  <form class="my-8 mx-auto max-w-md flex flex-col gap-6">
-    <h2 class="mb-8 text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
+  <form class="my-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <h2 class="col-span-full mb-8 text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl">
       Saada andmed
     </h2>
-    <form-input
-      id="company-name"
-      autofocus
-      label="Arve esitada äriühingule nimega"
-    />
-    <form-input
-      id="company-y"
-      label="Arve saaja äriühingu y-tunnus"
-    />
-    <form-input
-      id="ocject-number"
-      label="Objekti number/aadress"
-    />
-    <form-input
-      id="description"
-      label="Töö kirjeldus ja selgitus"
-    />
-    <form-input
-      id="price"
-      label="Töö hind kokku mis summas arve esitada"
-    />
-    <form-input
-      id="first-name"
-      label="Eesnimi"
-    />
-    <form-input
-      id="last-name"
-      label="Perekonnanimi"
-    />
-    <form-input
-      id="e-mail"
-      label="E-mail"
-    />
-    <form-input
-      id="phone"
-      label="Telefoninumber"
-    />
-    <form-input
-      id="address"
-      label="Aadress"
-    />
-    <form-input
-      id="vero-number"
-      label="Veronumber"
-    />
-    <form-input
-      id="id-number"
-      label="Henkilötunnus"
-    />
-    <form-input
-      id="iban"
-      label="Panga kontonumber"
-    />
-    <form-input
-      id="bic"
-      label="Panga BIC"
-    />
-    <form-input
-      id="info"
-      label="Muu selgitus"
-      rows="5"
-      type="textarea"
-    />
-    <form-input
-      id="km"
-      label="Kilometrit (0,53 eur / km)"
-    />
-    <form-input
-      id="Full-day-allowance"
-      label="Kokopäiväraha (48,00 eur)"
-    />
-    <form-input
-      id="partial-day-allowance"
-      label="Osapäiväraha (22,00 eur)"
-    />
-    <form-input
-      id="meal-compensation"
-      label="Aterikorvaus (12,oo eur)"
-    />
 
-    <form-button>Saada</form-button>
+    <div class="flex flex-col gap-6">
+      <h3 class="text-xl font-bold text-purple-800">
+        Arve saaja
+      </h3>
+
+      <form-input
+        id="company-name"
+        autofocus
+        label="Äriühingu nimi"
+      />
+      <form-input
+        id="company-y"
+        label="Äriühingu y-tunnus"
+      />
+      <form-input
+        id="ocject-number"
+        label="Objekti number/aadress"
+      />
+      <form-input
+        id="description"
+        label="Töö kirjeldus ja selgitus"
+      />
+      <form-input
+        id="price"
+        label="Töö hind kokku mis summas arve esitada"
+      />
+    </div>
+
+    <div class="flex flex-col gap-6">
+      <h3 class="text-xl font-bold text-purple-800">
+        Palga saaja
+      </h3>
+
+      <form-input
+        id="first-name"
+        label="Eesnimi"
+      />
+      <form-input
+        id="last-name"
+        label="Perekonnanimi"
+      />
+      <form-input
+        id="e-mail"
+        label="E-mail"
+      />
+      <form-input
+        id="phone"
+        label="Telefoninumber"
+      />
+      <form-input
+        id="address"
+        label="Aadress"
+      />
+      <form-input
+        id="vero-number"
+        label="Veronumber"
+      />
+      <form-input
+        id="id-number"
+        label="Henkilötunnus"
+      />
+      <form-input
+        id="iban"
+        label="Panga kontonumber"
+      />
+      <form-input
+        id="bic"
+        label="Panga BIC"
+      />
+      <form-input
+        id="info"
+        label="Muu selgitus"
+        rows="5"
+        type="textarea"
+      />
+    </div>
+
+    <div class="flex flex-col gap-6">
+      <h3 class="text-xl font-bold text-purple-800">
+        ?????
+      </h3>
+
+      <form-input
+        id="km"
+        label="Kilometrit (0,53 eur / km)"
+      />
+      <form-input
+        id="Full-day-allowance"
+        label="Kokopäiväraha (48,00 eur)"
+      />
+      <form-input
+        id="partial-day-allowance"
+        label="Osapäiväraha (22,00 eur)"
+      />
+      <form-input
+        id="meal-compensation"
+        label="Aterikorvaus (12,oo eur)"
+      />
+    </div>
+
+    <div class="col-span-full text-center">
+      <form-button class="max-w-lg">
+        Saada
+      </form-button>
+    </div>
   </form>
 </template>
