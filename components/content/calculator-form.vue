108d059f46698aaf367f0e214cb13e00bdbb374d
Argo Roots <argo@roots.ee>
2023-12-12 13:06:01 +0200

    Calculator form

---
+++ components/content/calculator-form.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <form
+    action="/api/send.php"
+    class="my-8 mx-auto max-w-md flex flex-col gap-8"
+    method="POST"
+  >
+    <div class="flex flex-col gap-6">
+      <form-input
+        id="price"
+        label="Töö hind kokku mis summas arve esitada"
+      />
+
+      <form-input
+        id="km"
+        label="Kilometrit (0,53€ / km)"
+      />
+      <form-input
+        id="Full-day-allowance"
+        label="Kokopäiväraha (48,00€)"
+      />
+      <form-input
+        id="partial-day-allowance"
+        label="Osapäiväraha (22,00€)"
+      />
+      <form-input
+        id="meal-compensation"
+        label="Aterikorvaus (12,00€)"
+      />
+    </div>
+  </form>
+</template>
