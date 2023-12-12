46baaed0c071ea9d153976f4cc066ffad43ac8cf
Argo Roots <argo@roots.ee>
2023-12-12 14:22:14 +0200

    Fix formatting

--- public/api/send.php
+++ public/api/send.php
@@ -1,6 +1,6 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $subject = 'Form submission';
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $subject = "Form submission';
     $toEmail = 'argo@roots.ee';
     $fromEmail = 'valitoimisto.fi <web@valitoimisto.fi>';
 
@@ -25,12 +25,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $body .= "<p><strong>" . str_replace("-", " ", $key) . ":<br></strong>" . htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . "</p>";
     }
 
-    $body .= '</body></html>';
+    $body .= "</body></html>\r\n";
 
     // Process file uploads
     foreach ($_FILES as $fileKey => $file) {
-      $filename = filter_var($file['name'], FILTER_SANITIZE_STRING);
-      $fileContent = chunk_split(base64_encode(file_get_contents($file['tmp_name'])));
+      $filename = filter_var($file["name"], FILTER_SANITIZE_STRING);
+      $fileContent = chunk_split(base64_encode(file_get_contents($file["tmp_name"])));
       $body .= "--$boundary\r\n";
       $body .= "Content-Type: application/octet-stream\r\n";
       $body .= "Content-Disposition: attachment; filename=\"$filename\"\r\n";
@@ -50,6 +50,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       exit();
     }
 } else {
-    echo 'Invalid request method.';
+    echo "Invalid request method.";
 }
 ?>
