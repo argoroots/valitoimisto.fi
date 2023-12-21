28dfc9d07b52a37c43688e3bc048668f58e83a99
Argo Roots <argo@roots.ee>
2023-12-21 14:54:46 +0200

    Update logos layout

--- components/content/logos.vue
+++ components/content/logos.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="mt-28">
+  <div class="mt-28 bg-white border border-slate-200">
     <slot />
   </div>
 </template>
 
 <style scoped>
+:deep(h2) {
+  @apply pt-8;
+  @apply text-center;
+}
+
 :deep(p) {
   @apply block;
-  @apply mt-8;
   @apply px-4;
-  @apply py-4 sm:py-16;
+  @apply py-4 sm:py-8;
   @apply grid;
   @apply grid-cols-2 sm:grid-cols-3;
   @apply justify-center;
   @apply items-center;
   @apply gap-4 sm:gap-16;
-  @apply bg-white;
 }
+
 :deep(img) {
   @apply inline-block;
   @apply w-full;
