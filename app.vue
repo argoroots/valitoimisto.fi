dc3af7f9e05a247e44ccda0f85a404004c79001f
Argo Roots <argo@roots.ee>
2023-12-27 09:45:33 +0200

    Update dependencies

--- app.vue
+++ app.vue
@@ -5,7 +5,5 @@ useHead({
 </script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <NuxtPage />
 </template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -7,21 +7,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <main class="mx-auto">
-    <ContentDoc>
-      <template #not-found>
-        <div class="w-full text-center">
-          <p class="text-base font-semibold text-purple-900">
-            404
-          </p>
-          <h1 class="mt-4 text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl">
-            Page not found
-          </h1>
-          <p class="mt-6 text-base leading-7 text-slate-600">
-            Sorry, we couldn’t find the page you’re looking for.
-          </p>
-        </div>
-      </template>
-    </ContentDoc>
-  </main>
+  <NuxtLayout>
+    <main class="mx-auto">
+      <ContentDoc>
+        <template #not-found>
+          <div class="w-full text-center">
+            <p class="text-base font-semibold text-purple-900">
+              404
+            </p>
+            <h1 class="mt-4 text-3xl font-bold tracking-tight text-slate-900 sm:text-5xl">
+              Page not found
+            </h1>
+            <p class="mt-6 text-base leading-7 text-slate-600">
+              Sorry, we couldn’t find the page you’re looking for.
+            </p>
+          </div>
+        </template>
+      </ContentDoc>
+    </main>
+  </NuxtLayout>
 </template>
