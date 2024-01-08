f149652d8d6c82916a3577446ac2951ad6504a07
Argo Roots <argo@roots.ee>
2024-01-08 15:56:54 +0200

    Add hero component translation

--- components/content/hero.vue
+++ components/content/hero.vue
@@ -1,3 +1,7 @@
+<script setup>
+const { t } = useI18n()
+</script>
+
 <template>
   <div class="-m-6 md:m-0 md:mt-6 relative isolate overflow-hidden bg-slate-900 px-6 shadow-2xl md:rounded-3xl md:px-16 lg:flex lg:gap-x-20 lg:px-24 pt-0">
     <svg viewBox="0 0 1024 1024" class="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] md:left-full md:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0" aria-hidden="true">
@@ -14,14 +18,19 @@
       <slot />
       <div class="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
         <a
-          href="/send"
+          :href="t('navSendDataUrl')"
           class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-slate-900 shadow-sm hover:opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white no-underline"
-        >Saada andmed</a>
+        >
+          {{ t('navSendData') }}
+        </a>
 
         <a
-          href="/calculator"
+          :href="t('navCalculatorUrl')"
           class="rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:opacity-80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white no-underline"
-        >Kalkulaator <span aria-hidden="true">→</span></a>
+        >
+          {{ t('navCalculator') }}
+          <span aria-hidden="true">→</span>
+        </a>
       </div>
     </div>
   </div>
